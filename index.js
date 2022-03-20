8e858805ed49286d37bb2f6c94be936d3dd0331e
Nikolai Osipov <Ayakashi2.0@yandex.ru>
2022-03-20 17:48:17 +0300

    lesson 1.1

--- index.js
+++ index.js
@@ -1,51 +1,10 @@
 const colors = require('colors');
 
-// let foo = 'Hello World!';
-// console.log(foo);
-// console.log(colors.red(foo));
-
-// let n = 10;
-//
-// nextP:
-//     for (let i = 2; i <= n; i++) {
-//         for (let j = 2; j < i; j++) {
-//             if (i % j == 0) continue nextP;
-//         }
-//         console.log(i);
-//     }
-
-// const primeNumber = () => {
-//     let n = 10;
-//
-//     for (let i = 3; i <= n; i++) {
-//         let foo = true;
-//
-//         for (let j = 2; j < i; j++) {
-//             if (i % j === 0) {
-//                 foo = false;
-//                 break;
-//             }
-//         }
-//         if (foo) console.log(i);
-//     };
-// };
-// primeNumber();
-
-
-const color = () => {
-    if (i % 3 === 0) {
-        console.log(colors.green(i));
-    } else if ((i + 1) % 3 === 0) {
-        console.log(colors.yellow(i));
-    } else  {
-        console.log(colors.red(i));
-    }
-};
-
 const primeNumber = () => {
-    let n = 10;
+    let start = 2;
+    let end = 100;
 
-    for (let i = 3; i <= n; i++) {
+    for (let i = start; i <= end; i++) {
         let foo = true;
 
         for (let j = 2; j < i; j++) {
@@ -54,7 +13,21 @@ const primeNumber = () => {
                 break;
             }
         }
-        if (foo) console.log(i)
+
+        if (foo) {
+            printColor(i)
+        }
+
+    }
+};
+
+const printColor = (i) => {
+    if (i % 3 === 0) {
+        console.log(colors.yellow(i));
+    } else if ((i + 1) % 3 === 0) {
+        console.log(colors.green(i));
+    } else  {
+        console.log(colors.red(i));
     }
 };
 
