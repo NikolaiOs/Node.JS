ea1f4d0f36757deaa05a4b8d5a16d1b7c9e92939
Nikolai Osipov <Ayakashi2.0@yandex.ru>
2022-03-31 23:28:07 +0300

    lesson 4.3

--- cli.js
+++ cli.js
@@ -1,48 +1,49 @@
-const fs = require("fs");
+#!/c/nodejs/node
+
+const fs = require("fs/promises");
 const path = require("path");
 const inquirer = require("inquirer");
 const yargs = require("yargs");
 
-// const [ fileName ] = process.argv.slice(2);
-//
-// fs.readFile(fileName, 'utf-8', (err, data) => {
-//     if (err)
-//         return console.log(err);
-//     console.log(data);
-// });
+let currentDirectory = process.cwd();
 
-const currentDirectory = process.cwd();
+const options = yargs
+    .positional('d', {
+        describe: 'Path to directory',
+        default: process.cwd(),
+    })
+    .positional('p', {
+        describe: 'Pattern',
+        default: '',
+    }).argv;
 
-// const options = yargs
-//     .positional("p", {describe: 'Path', default: process.cwd()})
-//     .argv;
-// console.log(options);
-//
-// class item {
-//     constructor(path, fileName) {
-//         this.path = path;
-//         this.fileName = fileName;
-//     }
-// }
+console.log(options);
 
-// const isFile = fileName => {
-//
-//     return fs.lstatSync(fileName).isFile();
-// };
-//
-// const list = fs.readdirSync(currentDirectory).filter(isFile);
-// inquirer
-//     .prompt([
-//         {
-//             name: "fileName",
-//             type: "list",
-//             message: "Choose file:",
-//             choices: list,
-//         },
-//     ])
-//     .then((answer) => {
-//         const filePath = path.join(currentDirectory, answer.fileName);
-//         fs.readFile(filePath,'utf8', (err, data) => {
-//             console.log(data);
-//         });
-//     });
+class ListItem {
+    constructor(path, fileName) {
+        this.path = path;
+        this.fileName = fileName;
+    }
+};
+
+const run = async () => {
+    const list = await fs.readdir(currentDirectory);
+    const items = list.map(fileName =>
+        new ListItem(path.join(currentDirectory, fileName), fileName));
+
+    const item = await inquirer.prompt([
+            {
+                name: 'fileName',
+                type: 'list',
+                message: `Выберите файл: ${currentDirectory}`,
+                choices: items.map(item => ({ name: item.fileName, value: item })),
+            }
+        ])
+        .then(answer => answer.fileName);
+
+    const data = await fs.readFile(item.path, 'utf-8');
+
+    console.log(data);
+};
+
+run();
